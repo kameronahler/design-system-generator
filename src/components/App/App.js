79662710ad6c9d0c16073f6209ee0fbae50eea54
Kameron <kamahler@gmail.com>
2021-01-14 21:47:56 -0700

    create parent typography component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from 'react'
-import Typography from '../Typography/Typography'
+import Typography from '../TypographyElements/TypographyElements'
 
 const initialContext = {
   typographyElements: ['h1'],
