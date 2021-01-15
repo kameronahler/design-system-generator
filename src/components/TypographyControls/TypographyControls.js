1f3ea1abede6034c12f134322501f3f595869b39
Kameron <kamahler@gmail.com>
2021-01-15 00:13:21 -0700

    enumerate individual typography controls

--- src/components/TypographyControls/TypographyControls.js
+++ src/components/TypographyControls/TypographyControls.js
@@ -2,43 +2,28 @@ import React from 'react'
 import TypographyControlsCheckbox from '../TypographyControlsCheckbox/TypographyControlsCheckbox'
 
 export default function TypographyControls() {
+  const TYPOGRAPHY_ELEMENT_CHOICES = [
+    { id: 'heading-1', labelText: 'Heading 1', value: 'h1' },
+    { id: 'heading-2', labelText: 'Heading 2', value: 'h2' },
+    { id: 'heading-3', labelText: 'Heading 3', value: 'h3' },
+    { id: 'heading-4', labelText: 'Heading 4', value: 'h4' },
+    { id: 'heading-5', labelText: 'Heading 5', value: 'h5' },
+    { id: 'heading-6', labelText: 'Heading 6', value: 'h6' },
+    { id: 'paragraph', labelText: 'Paragraph', value: 'p' },
+  ]
+
   return (
     <div>
-      <TypographyControlsCheckbox
-        id={'heading-1'}
-        labelText={'Heading 1'}
-        value={'h1'}
-      />
-      <TypographyControlsCheckbox
-        id={'heading-2'}
-        labelText={'Heading 2'}
-        value={'h2'}
-      />
-      <TypographyControlsCheckbox
-        id={'heading-3'}
-        labelText={'Heading 3'}
-        value={'h3'}
-      />
-      <TypographyControlsCheckbox
-        id={'heading-4'}
-        labelText={'Heading 4'}
-        value={'h4'}
-      />
-      <TypographyControlsCheckbox
-        id={'heading-5'}
-        labelText={'Heading 5'}
-        value={'h5'}
-      />
-      <TypographyControlsCheckbox
-        id={'heading-6'}
-        labelText={'Heading 6'}
-        value={'h6'}
-      />
-      <TypographyControlsCheckbox
-        id={'paragraph'}
-        labelText={'Paragraph'}
-        value={'p'}
-      />
+      {TYPOGRAPHY_ELEMENT_CHOICES.map(el => {
+        return (
+          <TypographyControlsCheckbox
+            key={el.id}
+            id={el.id}
+            labelText={el.labelText}
+            value={el.value}
+          />
+        )
+      })}
     </div>
   )
 }
