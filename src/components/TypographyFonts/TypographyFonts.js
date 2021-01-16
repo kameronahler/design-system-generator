c0604fa90430d67118b2442cffbab0908c904862
Kameron <kamahler@gmail.com>
2021-01-15 23:40:13 -0700

    fetch google fonts into select input, apply body style

--- src/components/TypographyFonts/TypographyFonts.js
+++ src/components/TypographyFonts/TypographyFonts.js
@@ -1,9 +1,65 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-const googleAPI = process.env.GOOGLE_DEV_API
+const GOOGLE_API = process.env.GOOGLE_DEV_API
+const GOOGLE_FONTS_URL = `https://www.googleapis.com/webfonts/v1/webfonts?key=${GOOGLE_API}`
 
 export default function TypographyFonts() {
-  console.log(googleAPI)
+  // todo: memoize google font data?
+  const [fontData, setFontData] = useState([])
+  const [currentFont, setCurrentFont] = useState(null)
 
-  return <>fonts</>
+  const axiosFontData = async () => {
+    try {
+      const res = await axios.get(GOOGLE_FONTS_URL)
+      setFontData(res.data.items)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  const handleFormSubmit = e => {
+    e.preventDefault()
+  }
+  const handleSelectChange = e => {
+    const currentOption = e.currentTarget.querySelector('option:checked')
+    const newState = {
+      family: e.currentTarget.value,
+      href: currentOption.dataset.href,
+    }
+
+    // todo: create font face
+    setCurrentFont(newState)
+  }
+
+  const applyCurrentFontFamily = () => {
+    document.body.style.fontFamily = `${currentFont.family}, sans-serif`
+  }
+
+  useEffect(() => {
+    axiosFontData()
+
+    if (currentFont) {
+      applyCurrentFontFamily()
+    }
+  }, [currentFont])
+
+  return (
+    <>
+      <form onSubmit={handleFormSubmit}>
+        <select onChange={handleSelectChange}>
+          <option default>
+            {fontData.length > 0 ? 'Select a font' : 'Loading...'}
+          </option>
+          {fontData.map((el, i) => {
+            return (
+              <option key={i} data-href={el.files.regular} value={el.family}>
+                {el.family}
+              </option>
+            )
+          })}
+        </select>
+      </form>
+    </>
+  )
 }
